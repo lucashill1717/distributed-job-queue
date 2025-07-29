550c744cf436cf577047537f022d18146bdc8499
Lucas Hill <lucashill1717@gmail.com>
2025-07-29 16:37:04 -0600

    client/server specification

--- src/main.rs
+++ src/main.rs
@@ -16,17 +16,20 @@ fn main() {
     let args: Vec<String> = args().collect();
     let file_name: &String = match args.get(1) {
         None => {
-            eprintln!("Job file required as first argument. Exiting.");
-            exit(1);
+            println!("Running instance as client.");
+            return
         },
-        Some(file_name) => file_name
+        Some(file_name) => {
+            println!("Running instance as server.");
+            file_name
+        }
     };
 
     let path: &Path = Path::new(file_name);
     let mut file: File = match File::open(&path) {
         Err(why) => {
             eprintln!("Couldn't open {}: {}", path.display(), why);
-            exit(2);
+            exit(1);
         },
         Ok(file) => file
     };
@@ -34,13 +37,13 @@ fn main() {
     let mut file_contents: String = String::new();
     if let Err(why) = file.read_to_string(&mut file_contents) {
         eprintln!("Couldn't read from {}: {}", path.display(), why);
-        exit(3);
+        exit(2);
     }
 
     let user_input: UserInput = match serde_json::from_str(&file_contents) {
         Err(why) => {
             eprintln!("Parsing input {} failed: {}", path.display(), why);
-            exit(4);
+            exit(3);
         },
         Ok(user_input) => user_input
     };
