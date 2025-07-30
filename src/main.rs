bd075967230bfcbf9c27ee6cd2eb36731a3d3b7f
Lucas Hill <lucashill1717@gmail.com>
2025-07-30 15:26:52 -0600

    proper exit coding

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,26 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 use std::path::Path;
-use std::process;
+use std::process::ExitCode;
 
 mod client;
 mod server;
 
-fn main() {
+fn main() -> ExitCode {
     let args: Vec<String> = env::args().collect();
     let file_name: &String = match args.get(1) {
         None => {
             println!("Running instance as client.");
-            client::client();
-            return;
+            match client::client() {
+                Err(why) => {
+                    eprintln!("Client instance terminated unexpectedly: {}", why);
+                    return ExitCode::FAILURE;
+                }
+                Ok(_) => {
+                    println!();
+                    return ExitCode::SUCCESS;
+                }
+            };
         }
         Some(file_name) => {
             println!("Running instance as server.");
@@ -25,7 +33,7 @@ fn main() {
     let mut file: File = match File::open(&path) {
         Err(why) => {
             eprintln!("Couldn't open {}: {}", path.display(), why);
-            process::exit(1);
+            return ExitCode::FAILURE;
         }
         Ok(file) => file
     };
@@ -33,16 +41,26 @@ fn main() {
     let mut file_contents: String = String::new();
     if let Err(why) = file.read_to_string(&mut file_contents) {
         eprintln!("Couldn't read from {}: {}", path.display(), why);
-        process::exit(2);
+        return ExitCode::FAILURE;
     }
 
     let user_input: server::UserInput = match serde_json::from_str(&file_contents) {
         Err(why) => {
             eprintln!("Parsing input {} failed: {}", path.display(), why);
-            process::exit(3);
+            return ExitCode::FAILURE;
         }
         Ok(user_input) => user_input
     };
 
-    server::server(user_input);
+    match server::server(user_input) {
+        Err(why) => {
+            eprintln!("Server instance terminated unexpectedly: {}", why);
+            return ExitCode::FAILURE;
+        }
+        Ok(_) => {
+            println!("Server shutdown successfully.");
+        }
+    };
+
+    return ExitCode::SUCCESS;
 }
