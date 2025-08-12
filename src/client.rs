c610b8a2e009f6cbf238bbb75478cba28e619bb7
Lucas Hill <lucashill1717@gmail.com>
2025-08-12 17:55:00 -0600

    import all from messages

--- src/client.rs
+++ src/client.rs
@@ -8,7 +8,7 @@ use std::{
     net::TcpStream
 };
 
-use crate::messages;
+use crate::messages::*;
 
 #[derive(Deserialize)]
 pub struct ClientInfo {
@@ -16,7 +16,7 @@ pub struct ClientInfo {
 }
 
 /// Sends messages::Message with length-delimited encoding.
-fn send_message(stream: &mut TcpStream, message: messages::Message) -> std::io::Result<()> {
+fn send_message(stream: &mut TcpStream, message: Message) -> std::io::Result<()> {
     let encoded: Vec<u8> = bincode::serialize(&message)
         .map_err(|why| std::io::Error::new(ErrorKind::InvalidData, why))?;
     let length: [u8; 4] = (encoded.len() as u32).to_be_bytes();
@@ -28,7 +28,7 @@ fn send_message(stream: &mut TcpStream, message: messages::Message) -> std::io::
 }
 
 /// Reads stream into messages::Message with length-delimited encoding.
-fn read_message(stream: &mut TcpStream) -> std::io::Result<messages::Message> {
+fn read_message(stream: &mut TcpStream) -> std::io::Result<Message> {
     let mut length_buf = [0u8; 4];
     stream.read_exact(&mut length_buf)?;
 
@@ -36,7 +36,7 @@ fn read_message(stream: &mut TcpStream) -> std::io::Result<messages::Message> {
     let mut message_buf: Vec<u8> = vec![0u8; length as usize];
     stream.read_exact(&mut message_buf)?;
 
-    match bincode::deserialize::<messages::Message>(&message_buf) {
+    match bincode::deserialize::<Message>(&message_buf) {
         Ok(message) => Ok(message),
         Err(why) => Err(std::io::Error::new(ErrorKind::InvalidData, why))
     }
@@ -47,19 +47,19 @@ pub fn client(info: ClientInfo) -> std::io::Result<()> {
     let addr = format!("{}:20057", info.server_name);
 
     let mut stream = TcpStream::connect(addr)?;
-    let ready = messages::Message::Ready(messages::Ready::new(cpu_count));
+    let ready = Message::Ready(Ready::new(cpu_count));
     send_message(&mut stream, ready)?;
 
     let message = read_message(&mut stream)?;
     match message {
-        messages::Message::Job(job) => {
+        Message::Job(job) => {
             println!("Job received: {:?}", job);
         }
         _ => {}
     }
 
-    let map = HashMap::<u32, HashMap::<messages::Action, String>>::new();
-    let done= messages::Message::Done(messages::Done::new(map));
+    let map = HashMap::<u32, HashMap::<Action, String>>::new();
+    let done= Message::Done(Done::new(map));
     send_message(&mut stream, done)?;
 
     Ok(())
--- src/server.rs
+++ src/server.rs
@@ -6,14 +6,14 @@ use serde::Deserialize;
 use tokio::net::TcpListener;
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
-use crate::messages;
+use crate::messages::*;
 
 static JOB_ID_COUNTER: AtomicU32 = AtomicU32::new(0);
 
 #[derive(Deserialize, Debug)]
 pub struct ServerInfo {
     pub source: String,
-    pub actions: Vec<messages::Action>
+    pub actions: Vec<Action>
 }
 
 #[tokio::main]
@@ -25,16 +25,16 @@ pub async fn server(info: ServerInfo) -> std::io::Result<()> {
         tokio::spawn(async move {
             let mut framed = Framed::new(stream, LengthDelimitedCodec::new());
             while let Some(Ok(bytes)) = framed.next().await {
-                let message = bincode::deserialize::<messages::Message>(&bytes).unwrap();
+                let message = bincode::deserialize::<Message>(&bytes).unwrap();
                 match message {
-                    messages::Message::Ready(ready) => {
+                    Message::Ready(ready) => {
                         println!("Ready message received: {ready:?}");
                         let job_id: u32 = JOB_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-                        let job = messages::Message::Job(messages::Job::new(job_id, "".to_string(), vec![]));
+                        let job = Message::Job(Job::new(job_id, "".to_string(), vec![]));
                         let encoded: Vec<u8> = bincode::serialize(&job).unwrap();
                         framed.send( encoded.into()).await;
                     }
-                    messages::Message::Done(done) => {
+                    Message::Done(done) => {
                         println!("Done message received: {done:?}");
                     }
                     _ => {}
