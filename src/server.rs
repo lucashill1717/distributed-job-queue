bf44533fc80bb089aeec756a52c573037fdd8a11
Lucas Hill <lucashill1717@gmail.com>
2025-08-13 09:32:40 -0600

    moved thread logic into function

--- src/server.rs
+++ src/server.rs
@@ -6,7 +6,7 @@ use std::sync::{
 use bincode;
 use futures::{SinkExt, StreamExt};
 use serde::Deserialize;
-use tokio::net::TcpListener;
+use tokio::net::{TcpListener, TcpStream};
 use tokio_util::codec::{Framed, LengthDelimitedCodec};
 
 use crate::messages::*;
@@ -19,6 +19,27 @@ pub struct ServerInfo {
     pub actions: Vec<Action>
 }
 
+async fn thread_runner(stream: TcpStream, cloned_actions: Arc<Vec<Action>>) {
+    let mut framed = Framed::new(stream, LengthDelimitedCodec::new());
+    while let Some(Ok(bytes)) = framed.next().await {
+        let message = bincode::deserialize::<Message>(&bytes).unwrap();
+        match message {
+            Message::Ready(ready) => {
+                println!("Ready message received: {ready:?}");
+                let job_id: u32 = JOB_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
+                let job = Message::Job(Job::new(job_id, "".to_string(), cloned_actions.to_vec()));
+                let encoded: Vec<u8> = bincode::serialize(&job).unwrap();
+                framed.send( encoded.into()).await;
+            }
+            Message::Done(done) => {
+                println!("Done message received: {done:?}");
+                break;
+            }
+            _ => {}
+        }
+    }
+}
+
 #[tokio::main]
 pub async fn server(info: ServerInfo) -> std::io::Result<()> {
     let actions = Arc::new(info.actions);
@@ -28,24 +49,7 @@ pub async fn server(info: ServerInfo) -> std::io::Result<()> {
         let (stream, _) = listener.accept().await?;
         let cloned_actions = Arc::clone(&actions);
         tokio::spawn(async move {
-            let mut framed = Framed::new(stream, LengthDelimitedCodec::new());
-            while let Some(Ok(bytes)) = framed.next().await {
-                let message = bincode::deserialize::<Message>(&bytes).unwrap();
-                match message {
-                    Message::Ready(ready) => {
-                        println!("Ready message received: {ready:?}");
-                        let job_id: u32 = JOB_ID_COUNTER.fetch_add(1, Ordering::Relaxed);
-                        let job = Message::Job(Job::new(job_id, "".to_string(), cloned_actions.to_vec()));
-                        let encoded: Vec<u8> = bincode::serialize(&job).unwrap();
-                        framed.send( encoded.into()).await;
-                    }
-                    Message::Done(done) => {
-                        println!("Done message received: {done:?}");
-                        break;
-                    }
-                    _ => {}
-                }
-            }
+            thread_runner(stream, cloned_actions).await;
         });
     }
 }
